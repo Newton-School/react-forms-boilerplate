ac372c7fd1d084f28304da67b68eafa26913ab38
RaghavSabby <ragsabharwal369@gmail.com>
2022-12-01 13:04:17 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -11,16 +11,12 @@ const App = () => {
         <section>
           <label>Username</label>
           <input type="text" name='username' />
-          <p>{formErrors.username}</p>
           <label>Email</label>
           <input type="email" name='email' />
-          <p>{formErrors.email}</p>
           <label>Password</label>
           <input type="password" name='password' />
-          <p>{formErrors.password}</p>
           <label>Contact Number</label>
           <input type="number" name='contactNo' />
-          <p>{formErrors.contactNo}</p>
           <button>Submit</button>
         </section>
       </form>
