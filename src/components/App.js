f82c1961f034eec89378062dcc61cf1eef57a869
RaghavSabby <ragsabharwal369@gmail.com>
2022-12-03 14:21:56 +0530

    edit error message html

--- src/components/App.js
+++ src/components/App.js
@@ -11,12 +11,16 @@ const App = () => {
         <section>
           <label>Username</label>
           <input type="text" name='username' />
+          {/* {<p className='username-error'></p>} */}
           <label>Email</label>
           <input type="email" name='email' />
+          {/* {<p className='email-error'></p>} */}
           <label>Password</label>
           <input type="password" name='password' />
+          {/* {<p className='password-error'></p>} */}
           <label>Contact Number</label>
           <input type="number" name='contactNo' />
+          {/* {<p className='contactNo-error'></p>} */}
           <button>Submit</button>
         </section>
       </form>
